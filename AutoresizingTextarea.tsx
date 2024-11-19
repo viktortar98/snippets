3085ce54233a69a441b67dfe92c3f475a88fb714
viktortar98 <72432479+viktortar98@users.noreply.github.com>
2024-11-19 22:18:17 +0100

    Update AutoresizingTextarea.tsx

--- AutoresizingTextarea.tsx
+++ AutoresizingTextarea.tsx
@@ -1,46 +1,56 @@
-import { JSX, onCleanup, onMount } from "solid-js";
+import { createEffect, createSignal, JSX, onCleanup, onMount } from "solid-js";
 
 export function AutoresizingTextarea(props: Omit<JSX.HTMLAttributes<HTMLTextAreaElement>, "ref">) {
   let textareaRef!: HTMLTextAreaElement;
+
   let lastContent = '';
+  function resize() {
+    const currentContent = textareaRef.value;
+    if (currentContent === lastContent) return;
+    lastContent = currentContent;
+
+    textareaRef.style.height = "0";
+    textareaRef.style.height = textareaRef.scrollHeight + "px";
+    textareaRef.style.resize = "none";
+  }
 
   onMount(() => {
-    function resize() {
-      const currentContent = textareaRef.value;
-      if (currentContent === lastContent) return;
-      lastContent = currentContent;
-
-      textareaRef.style.height = "0";
-      textareaRef.style.height = textareaRef.scrollHeight + "px";
-      textareaRef.style.resize = "none";
-    }
-
     resize();
-
-    let animationFrameId: number;
-
-    function startAnimation() {
-      void (function animationLoop() {
-        resize();
-        animationFrameId = requestAnimationFrame(animationLoop);
-      })();
-    }
-
-    function stopAnimation() {
-      if (animationFrameId) {
-        cancelAnimationFrame(animationFrameId);
+    createFocusEffect(textareaRef, (isFocused) => {
+      if (isFocused) {
+        createAnimationFrameListener(resize);
       }
-    }
-
-    textareaRef.addEventListener('focus', startAnimation);
-    textareaRef.addEventListener('blur', stopAnimation);
-
-    onCleanup(() => {
-      textareaRef.removeEventListener('focus', startAnimation);
-      textareaRef.removeEventListener('blur', stopAnimation);
-      stopAnimation();
     });
   });
 
+
   return <textarea {...props} ref={textareaRef} />;
+}
+
+
+function createFocusEffect(element: HTMLTextAreaElement, effect: (isFocused: boolean) => void) {
+  const [isFocused, setIsFocused] = createSignal(false);
+
+  onMount(() => {
+    element.addEventListener('focus', () => setIsFocused(true));
+    element.addEventListener('blur', () => setIsFocused(false));
+  });
+
+  createEffect(() => {
+    effect(isFocused());
+  });
+}
+
+
+function createAnimationFrameListener(effect: () => void) {
+  let animationFrameId: number;
+
+  void (function animationLoop() {
+    effect();
+    animationFrameId = requestAnimationFrame(animationLoop);
+  })();
+
+  onCleanup(() => {
+    cancelAnimationFrame(animationFrameId);
+  });
 }
